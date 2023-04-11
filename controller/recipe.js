5c47bc7f2e3a7e6e77616779a9fb4da7530ac3b7
Yoochan Hwang <yoochan1868@gmail.com>
2023-04-11 10:04:08 +0900

    Updated getRecipes method in controller

--- controller/recipe.js
+++ controller/recipe.js
@@ -1,20 +1,29 @@
-import * as recipeRepository from '../data/recipe.js';
+import * as recipeRepository from "../data/recipe.js";
 
 export async function getRecipes(req, res) {
   const pageInfo = req.query;
   const page = parseInt(pageInfo.page);
-  const pageSize = parseInt(pageInfo.pagSize);
+  const pageSize = parseInt(pageInfo.pageSize);
+  
   try {
     let start = 0;
 
     if (page <= 0) {
-      page = 1;
+      start = 1;
     } else {
       start = (page - 1) * pageSize;
     }
-    const data = await recipeRepository.getRecipes(page, pageSize);
+
+    const total = await recipeRepository.getRecipeTotalCnt();
+
+    if (page > Math.round(total[0].cnt / pageSize)) {
+      res.status(200).json([]);
+    }
+
+    const data = await recipeRepository.getRecipes(start, pageSize);
+
+    res.status(200).json(data);
   } catch (error) {
-    throw error;
+    throw error;  
   }
-  res.status(200).json(data);
 }
--- data/recipe.js
+++ data/recipe.js
@@ -1,9 +1,16 @@
 import { db } from "../db/database.js";
 
-const SELECT_RECIPE = 'SELECT * FROM recipe LIMIT 0, 5'
+const SELECT_RECIPE = 'SELECT * FROM recipe';
+const SELECT_TOTAL_CNT_RECIPE = 'SELECT count(*) as cnt FROM recipe';
+
+export async function getRecipeTotalCnt() {
+  return db
+    .execute(`${SELECT_TOTAL_CNT_RECIPE}`) //
+    .then((result) => result[0]);
+}
 
 export async function getRecipes(page, pageSize) {
   return db
-    .execute(`${SELECT_RECIPE}`) //
+    .execute(`${SELECT_RECIPE} LIMIT ${page}, ${pageSize}`) //
     .then((result) => result[0]);
 }
